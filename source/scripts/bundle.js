7c640a5d2a9439296f8cce44788e140abfcdbf22
Evan Lecklider <evan.lecklider@gmail.com>
2016-03-11 11:09:17 -0800

    Simplified JS and added a banner to the home page.

--- source/scripts/bundle.js
+++ source/scripts/bundle.js
@@ -2,32 +2,13 @@
 
 document.addEventListener('DOMContentLoaded', function(ev) {
   /* check for some nodes we might care about */
-  var pattern = document.getElementById('pattern'),
-      list    = document.getElementById('post-list');
+  var patterns = document.querySelectorAll('[data-pattern]');
 
-  if (pattern) {
-    var title  = document.title,
-        geopat = GeoPattern.generate(title);
+  for (var patternIndex = patterns.length - 1; patternIndex >= 0; patternIndex--) {
+    var element = patterns[patternIndex],
+        pattern = element.getAttribute('data-pattern'),
+        geopat = GeoPattern.generate(pattern);
 
-    /* set that pattern */
-    pattern.style.backgroundImage = geopat.toDataUrl();
-  }
-
-  if (list) {
-    var items = list.getElementsByTagName('li');
-
-    for (var i = items.length - 1; i >= 0; i--) {
-      /* create a span and insert it before the link */
-      var item = items[i],
-          link = item.getElementsByTagName('a')[0],
-          span = document.createElement('span'),
-          pat  = GeoPattern.generate(link.textContent + ' | Evan Lecklider');
-
-      /* spool up our span and then insert it */
-      span.classList.add('pattern');
-      span.style.backgroundImage = pat.toDataUrl();
-
-      item.insertBefore(span, link);
-    }
+    element.style.backgroundImage = geopat.toDataUrl();
   }
 });
